3ec15f169638853dfcf1bde61f644717e3e08195
Wiktor Sikora <wiktor.sikora@kodegenix.pl>
2017-09-07 16:24:15 +0200

    Refactor, api change

--- src/wetty.js
+++ src/wetty.js
@@ -1,36 +1,41 @@
 import {lib, hterm} from './hterm_all';
 
+const Q = require('q')
 let term = null;
 let buf = '';
 
-export function create(socket, terminalId) {
-    function onSshStarted() {
 
-        console.log('ssh.started event received')
+const wetty = {
+    init: function (socket, terminalId) {
+        const deferred = Q.defer();
+        const self = this;
+        self.socket = socket;
+        self.terminalId = terminalId;
 
-        function socketOnOutput (data) {
-            if (!term) {
-                buf += data;
-                return;
+        class Command {
+            constructor(argv) {
+                this.argv_ = argv;
+                this.io = null;
+                this.pid_ = -1;
             }
-            term.io.writeUTF8(data);
-        }
-        function socketOnLogout() {
-            console.log('on logout')
 
-            socket.removeListener('output', socketOnOutput);
-            socket.removeListener('ssh.started', onSshStarted);
-            socket.removeListener('ssh.disconnect', socketOnSshDisconnect);
-            socket.removeListener('logout', socketOnLogout);
-        }
-        function socketOnSshDisconnect() {
-            console.log('on ssh.disconnect')
+            run() {
+                this.io = this.argv_.io.push();
+                this.io.onVTKeystroke = this.sendString_.bind(this);
+                this.io.sendString = this.sendString_.bind(this);
+                this.io.onTerminalResize = this.onTerminalResize.bind(this);
+            }
 
-            socket.removeListener('output', socketOnOutput);
-            socket.removeListener('ssh.started', onSshStarted);
-            socket.removeListener('logout', socketOnLogout);
-            socket.removeListener('ssh.disconnect', socketOnSshDisconnect);
+            sendString_(str) {
+                socket.emit('input', str);
+            }
+
+            onTerminalResize(col, row) {
+                socket.emit('resize', {col, row});
+            }
         }
+
+        // console.log('ssh.started event received')
 
         lib.init(() => {
             hterm.defaultStorage = new lib.Storage.Local();
@@ -50,51 +55,79 @@ export function create(socket, terminalId) {
             term.scrollPort_.screen_.setAttribute('autocorrect', 'false');
             term.scrollPort_.screen_.setAttribute('autocomplete', 'false');
             term.scrollPort_.screen_.setAttribute('contenteditable', 'false');
+            term.setHeight(30)
+            term.setWidth(150)
 
-            term.runCommandClass(Wetty, document.location.hash.substr(1));
-            socket.emit('resize', {
-                col: term.screenSize.width,
-                row: term.screenSize.height,
-            });
+            term.runCommandClass(Command, document.location.hash.substr(1));
 
             if (buf && buf !== '') {
                 term.io.writeUTF8(buf);
                 buf = '';
             }
 
-            socket.on('output', socketOnOutput);
-
-            socket.on('logout', socketOnLogout);
-
-            socket.on('ssh.disconnect', socketOnSshDisconnect);
+            console.log('resolve')
+            self.term = term;
+            deferred.resolve();
         });
 
+
+        console.log('init')
+        // socket.on('ssh.started', onSshStarted);
+        return deferred.promise;
+    },
+    connect: function (config) {
+        const socket = this.socket;
+        const term = this.term;
+        const self = this;
+
+
+        function socketOnOutput(data) {
+            if (!term) {
+                buf += data;
+                return;
+            }
+            term.io.writeUTF8(data);
+        }
+
+        function socketOnLogout() { // TODO merge
+            console.log('on logout')
+
+            document.getElementById(self.terminalId).remove()
+
+            socket.removeListener('output', socketOnOutput);
+            // socket.removeListener('ssh.started', onSshStarted);
+            socket.removeListener('ssh.disconnect', socketOnSshDisconnect);
+            socket.removeListener('logout', socketOnLogout);
+        }
+
+        function socketOnSshDisconnect() {// TODO merge
+            console.log('on ssh.disconnect')
+            document.getElementById(self.terminalId).remove()
+
+            socket.removeListener('output', socketOnOutput);
+            // socket.removeListener('ssh.started', onSshStarted);
+            socket.removeListener('logout', socketOnLogout);
+            socket.removeListener('ssh.disconnect', socketOnSshDisconnect);
+        }
+
+        socket.emit('resize', {
+            col: term.screenSize.width,
+            row: term.screenSize.height,
+        });
+
+        socket.on('output', socketOnOutput);
+
+        socket.on('logout', socketOnLogout);
+
+        socket.on('ssh.disconnect', socketOnSshDisconnect);
+
+        socket.emit('ssh.start', config)
+
+    },
+
+    disconnect: function () {
+        this.socket.emit('ssh.disconnect', {})
+
     }
-
-    class Wetty {
-        constructor(argv) {
-            this.argv_ = argv;
-            this.io = null;
-            this.pid_ = -1;
-        }
-
-        run() {
-            this.io = this.argv_.io.push();
-            this.io.onVTKeystroke = this.sendString_.bind(this);
-            this.io.sendString = this.sendString_.bind(this);
-            this.io.onTerminalResize = this.onTerminalResize.bind(this);
-        }
-
-        sendString_(str) {
-            socket.emit('input', str);
-        }
-
-        onTerminalResize(col, row) {
-            socket.emit('resize', {col, row});
-        }
-    }
-
-    console.log('create')
-
-    socket.on('ssh.started', onSshStarted);
 }
+export {wetty};
